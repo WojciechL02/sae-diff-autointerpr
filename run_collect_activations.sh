a9ae2f7ff6a5425adfe3779b3b08cbbf519c84d2
mpiorczynski <m.piorczynski13@gmail.com>
2025-04-11 13:52:42 +0200

    Add script for caching activations

---
+++ run_collect_activations.sh
@@ -0,0 +1,8 @@
+python scripts/collect_activations_sdxl_images.py \
+    --hook_names down_blocks.2 \
+    --dataset_path /net/pr2/projects/plgrid/plggzzsn2025/coco \
+    --split validation \
+    --model_name stabilityai/sdxl-turbo \
+    --num_inference_steps 1 \
+    --unconditional True \
+    --max_num_examples 500
