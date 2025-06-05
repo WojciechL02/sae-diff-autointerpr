cf02a26d2a96f661b68266c5b9f924078d7dd864
Wojciech Łapacz <wojciech.lapacz02@gmail.com>
2025-06-05 20:19:41 +0200

    log norms and losses separately

--- run_sae_textual_inversion_sdxl.sh
+++ run_sae_textual_inversion_sdxl.sh
@@ -1,12 +1,13 @@
-sae_latent_idx=123
+sae_latent_idx=374
+lr=100.0
 
 sbatch slurm_scripts/submit_job.sh scripts/sae_textual_inversion_sdxl.py \
     --pretrained_model_name_or_path stabilityai/sdxl-turbo \
-    --sae_checkpoint_path /net/tscratch/people/plgpiorczynskim/sae-diff-autointerpr/checkpoints/coco2017/sdxl-turbo/batch_topk_expansion_factor16_k32_multi_topkFalse_auxk_alpha0.03125_output_249_output \
+    --sae_checkpoint_path /net/tscratch/people/plgwlapacz/projects/sae-diff-autointerpr/checkpoints/batch_topk_expansion_factor16_k32_multi_topkFalse_auxk_alpha0.03125_output_249_output \
     --sae_hookpoint down_blocks.2 \
-    --sae_latent_idx $sae_latent_idx \
-    --cached_activations_path /net/tscratch/people/plgpiorczynskim/sae-diff-autointerpr/activations/coco2017/sdxl-turbo/steps4 \
-    --topk_examples 5 \
+    --sae_latent_idx ${sae_latent_idx} \
+    --cached_activations_path /net/tscratch/people/plgwlapacz/projects/sae-diff-autointerpr/activations/activations/sdxl-turbo/steps4 \
+    --topk_examples 16 \
     --timestep 249 \
     --coco_dataset_path /net/pr2/projects/plgrid/plggzzsn2025/coco \
     --resolution 1024 \
@@ -14,16 +15,17 @@ sbatch slurm_scripts/submit_job.sh scripts/sae_textual_inversion_sdxl.py \
     --placeholder_token "<sae-concept>" \
     --initializer_token concept \
     --num_vectors 1 \
-    --sae_activation_loss l2 \
-    --sae_activation_loss_weight 1e-4 \
+    --sae_activation_loss l1 \
+    --sae_activation_loss_weight 1.0 \
+    --diffusion_loss_weight 0.0 \
+    --sae_max_feature_act 2.43 \
     --train_batch_size 1 \
     --gradient_accumulation_steps 4 \
-    --max_train_steps 500 \
-    --learning_rate 5e-04 \
-    --scale_lr \
+    --max_train_steps 2000 \
+    --learning_rate ${lr} \
     --lr_scheduler constant \
     --lr_warmup_steps 0 \
     --save_steps 100 \
-    --output_dir sae_textual_inversion_sdxl/sae_latent_idx${sae_latent_idx} \
+    --output_dir sae_textual_inversion_sdxl/sae_latent_idx${sae_latent_idx}_lr${lr} \
     --report_to wandb \
     --seed 42
