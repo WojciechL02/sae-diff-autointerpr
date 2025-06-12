848d8523f69b2d43951ae02ea63fe41fdefcfed9
mpiorczynski <m.piorczynski13@gmail.com>
2025-06-12 03:59:42 +0200

    Push ready-to-go scripts

--- run.sh
+++ run.sh
@@ -1,43 +1,5 @@
-#!/bin/bash
-sae_latent_idx=374
-
-set -e
-
-mkdir -p logs/
-eval "$(conda shell.bash hook)"
-conda activate zzsn
-export PYTHONPATH="${PYTHONPATH}:$(pwd)"
-
-# CUDA
-module load CUDA/12.4.0
-echo $CUDA_HOME
-
-python3 scripts/sae_textual_inversion_sdxl.py \
-    --pretrained_model_name_or_path stabilityai/sdxl-turbo \
-    --sae_checkpoint_path /net/tscratch/people/plgwlapacz/projects/sae-diff-autointerpr/checkpoints/batch_topk_expansion_factor16_k32_multi_topkFalse_auxk_alpha0.03125_output_249_output \
-    --sae_hookpoint down_blocks.2 \
-    --sae_latent_idx $sae_latent_idx \
-    --cached_activations_path /net/tscratch/people/plgwlapacz/projects/sae-diff-autointerpr/activations/activations/sdxl-turbo/steps4 \
-    --topk_examples 16 \
-    --timestep 249 \
-    --coco_dataset_path /net/pr2/projects/plgrid/plggzzsn2025/coco \
-    --resolution 1024 \
-    --learnable_property concept \
-    --placeholder_token "<sae-concept>" \
-    --initializer_token concept \
-    --num_vectors 1 \
-    --sae_activation_loss l1 \
-    --sae_activation_loss_weight 1.0 \
-    --diffusion_loss_weight 0.0 \
-    --sae_max_feature_act 2.43 \
-    --train_batch_size 1 \
-    --gradient_accumulation_steps 4 \
-    --max_train_steps 2000 \
-    --learning_rate 1e-04 \
-    --scale_lr \
-    --lr_scheduler constant \
-    --lr_warmup_steps 0 \
-    --save_steps 100 \
-    --output_dir sae_textual_inversion_sdxl/sae_latent_idx${sae_latent_idx} \
-    --report_to wandb \
-    --seed 42
+for lr in 1e-3 1e-2 1e-1 1 10 100; do
+    for wd in 0.0 1e-5 1e-3 1e-1; do
+        bash run_sae_textual_inversion_sdxl_add_to_prompt.sh $lr $wd
+    done
+done
--- run_sae_textual_inversion_sdxl_add_to_prompt.sh
+++ run_sae_textual_inversion_sdxl_add_to_prompt.sh
@@ -1,9 +1,8 @@
 learning_rate=$1
+weight_decay=$2
 sae_latent_idx=374
 
-# --concept_embeddings_path /net/tscratch/people/plgpiorczynskim/sae-diff-autointerpr/sae_textual_inversion_sdxl_add_to_prompt_pooled/laion_bigrams_all_concept_embeddings.pt \
-
-
+# --sae_max_feature_act 50 \
 sbatch slurm_scripts/submit_job.sh scripts/sae_textual_inversion_sdxl_add_to_prompt.py \
     --pretrained_model_name_or_path stabilityai/sdxl-turbo \
     --sae_checkpoint_path /net/tscratch/people/plgpiorczynskim/sae-diff-autointerpr/checkpoints/coco2017/sdxl-turbo/batch_topk_expansion_factor16_k32_multi_topkFalse_auxk_alpha0.03125_output_249_output \
@@ -15,12 +14,13 @@ sbatch slurm_scripts/submit_job.sh scripts/sae_textual_inversion_sdxl_add_to_pro
     --coco_dataset_path /net/pr2/projects/plgrid/plggzzsn2025/coco \
     --resolution 1024 \
     --concept_vocab_name mscoco \
-    --sae_activation_loss l1 \
+    --concept_embeddings_path /net/tscratch/people/plgpiorczynskim/sae-diff-autointerpr/sae_textual_inversion_sdxl_add_to_prompt_pooled_coco/mscoco_all_concept_embeddings.pt \
+    --sae_activation_loss l2 \
     --sae_activation_loss_weight 1.0 \
     --sae_loss_max_weight 1.0 \
-    --sae_loss_min_weight 1.0 \
+    --sae_loss_min_weight 50.0 \
     --diffusion_loss_weight 0.0 \
-    --sae_max_feature_act 10 \
+    --learnable_concept_embedding_wd ${weight_decay} \
     --train_batch_size 1 \
     --gradient_accumulation_steps 4 \
     --max_train_steps 1000 \
@@ -30,6 +30,6 @@ sbatch slurm_scripts/submit_job.sh scripts/sae_textual_inversion_sdxl_add_to_pro
     --save_steps 200 \
     --do_validation \
     --validation_steps 200 \
-    --output_dir "sae_textual_inversion_sdxl_add_to_prompt_pooled_coco/sae_latent_idx${sae_latent_idx}_${learning_rate}" \
+    --output_dir "sae_textual_inversion_sdxl_add_to_prompt_pooled_coco/sae_latent_idx${sae_latent_idx}_${learning_rate}_l1" \
     --report_to wandb \
     --seed 42
